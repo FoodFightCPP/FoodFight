b349f7b01f66b6743f3fc6f78fc2463eeaaed891
petersteele111 <petersteele111@gmail.com>
2021-03-02 10:06:34 -0500

    Add project files.

---
+++ FoodFight/FoodFight/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodFight.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public int Zip { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+
+    }
+}
---
+++ FoodFight/FoodFight/root.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FoodFight
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Root : TabbedPage
+    {
+        public Root()
+        {
+            InitializeComponent();
+            var pages = Children.GetEnumerator();
+            pages.MoveNext(); // First page
+            pages.MoveNext(); // Second page
+            pages.MoveNext(); // Third page
+            CurrentPage = pages.Current;
+        }
+    }
+}
