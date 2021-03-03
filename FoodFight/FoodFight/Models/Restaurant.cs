43db636d8829877ec5e506adc18f55e241ce6dbe
petersteele111 <petersteele111@gmail.com>
2021-03-02 22:46:17 -0500

    Added Restaurant POCO

--- FoodFight/FoodFight/Models/Restaurant.cs
+++ FoodFight/FoodFight/Models/Restaurant.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace FoodFight.Models
 {
-    class Restaurant
+    public class Restaurant
     {
+        public int RestId { get; set; }
+        public string FormattedAddress { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Name { get; set; }
+        public string OpenNow { get; set; }
+        public string Photo { get; set; }
+        public double Rating { get; set; }
+        public long Phone { get; set; }
+        public string Website { get; set; }
     }
 }
