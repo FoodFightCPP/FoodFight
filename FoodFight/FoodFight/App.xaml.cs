90d85df734c2f0c8b9363ad38a8134ab2cc6cc2a
petersteele111 <petersteele111@gmail.com>
2021-03-03 01:30:36 -0500

    Added Syncfusion Information

--- FoodFight/FoodFight/App.xaml.cs
+++ FoodFight/FoodFight/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FoodFight.Views.Forms;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,9 +9,10 @@ namespace FoodFight
     {
         public App()
         {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NDA2NTI3QDMxMzgyZTM0MmUzMEx4dUs5V2w0MHBHUzdndkZJV2RUUE9Ma01NQ2h5SVhQUEdFOHk2anRhT3c9");
             InitializeComponent();
 
-            MainPage = new Root();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
