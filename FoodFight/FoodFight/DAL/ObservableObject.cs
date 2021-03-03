6eedb5ae8e976add70416a4f579857472fae97fd
petersteele111 <petersteele111@gmail.com>
2021-03-03 00:54:31 -0500

    Added Observable Object Class

--- FoodFight/FoodFight/DAL/ObservableObject.cs
+++ FoodFight/FoodFight/DAL/ObservableObject.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace FoodFight.DAL
 {
-    class ObservableObject
+    public abstract class ObservableObject : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyCHanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
