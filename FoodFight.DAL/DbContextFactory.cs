674813aa9be7ae4a79d91a245b02b11f315a1e85
petersteele111 <petersteele111@gmail.com>
2021-03-21 04:05:00 -0400

    Added Required NuGet packeges for EF Core and created the DBContext Factory and main DB Context with connection string

--- FoodFight.DAL/DbContextFactory.cs
+++ FoodFight.DAL/DbContextFactory.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FoodFight.DAL
 {
-    class DbContextFactory
+    public class DbContextFactory : IDesignTimeDbContextFactory<FoodFightDbContext>
     {
+        public FoodFightDbContext CreateDbContext(string[] args = null)
+        {
 
+            var options = new DbContextOptionsBuilder<FoodFightDbContext>();
+            options.UseSqlServer("Server=tcp:cppfoodfightdb.database.windows.net,1433;Database=FoodFight;User ID=cppff;Password=Capstone1;Encrypt=True;TrustServerCertificate=True;");
+
+            return new FoodFightDbContext(options.Options);
+        }
     }
 }
--- FoodFight.DAL/FoodFightDbContext.cs
+++ FoodFight.DAL/FoodFightDbContext.cs
@@ -1,11 +1,39 @@
-﻿using System;
+﻿using FoodFight.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FoodFight.DAL
 {
-    public class FoodFightDbContext
+    public class FoodFightDbContext : DbContext
     {
+        public FoodFightDbContext(DbContextOptions options) : base(options)
+        {
 
+        }
+
+        // Independent Tables
+        public DbSet<User> Users { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Setting> Settings { get; set; }
+
+        // Join Tables - Users
+        public DbSet<ConnectedUser> ConnectedUsers { get; set; }
+        public DbSet<BlockedUser> BlockedUsers { get; set; }
+        public DbSet<UserSetting> UserSettings { get; set; }
+
+        // Join Tables - Restaurants
+        public DbSet<FavoriteRestaurant> FavoriteRestaurants { get; set; }
+        public DbSet<BlockedRestaurant> BlockedRestaurants { get; set; }
+
+        // Session Tables
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<SwipeList> SwipeLists { get; set; }
+
+        public DbSet<RejectedRestaurant> RejectedRestaurants { get; set; }
+        public DbSet<AcceptedRestaurant> AcceptedRestaurants { get; set; }
+
+        public DbSet<MatchedRestaurant> MatchedRestaurants { get; set; }
     }
 }
