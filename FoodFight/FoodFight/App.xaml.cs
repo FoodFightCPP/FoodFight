ced9c4f1bae7f9fb74ecdd53169f121e6260f352
petersteele111 <petersteele111@gmail.com>
2021-03-21 03:07:56 -0400

    Add project files.

---
+++ FoodFight/FoodFight/App.xaml.cs
@@ -0,0 +1,39 @@
+using FoodFight.ViewModels;
+using FoodFight.Views;
+using Prism;
+using Prism.Ioc;
+using Xamarin.Essentials.Implementation;
+using Xamarin.Essentials.Interfaces;
+using Xamarin.Forms;
+
+namespace FoodFight
+{
+    public partial class App
+    {
+        public App(IPlatformInitializer initializer)
+            : base(initializer)
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("MainPage?selectedTab=Home");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<IAppInfo, AppInfoImplementation>();
+
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+            containerRegistry.RegisterForNavigation<Profile, ProfileViewModel>();
+            containerRegistry.RegisterForNavigation<Settings, SettingsViewModel>();
+            containerRegistry.RegisterForNavigation<Contacts, ContactsViewModel>();
+            containerRegistry.RegisterForNavigation<Favorites, FavoritesViewModel>();
+            containerRegistry.RegisterForNavigation<Home, HomeViewModel>();
+            containerRegistry.RegisterForNavigation<Start, StartViewModel>();
+        }
+    }
+}
