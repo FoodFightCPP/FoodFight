5e781221a2581f8854d04caea7a2393f0ddb177d
petersteele111 <petersteele111@gmail.com>
2021-03-21 04:15:33 -0400

    Fleshed out GenericDataService

--- FoodFight.DAL/Services/GenericDataService.cs
+++ FoodFight.DAL/Services/GenericDataService.cs
@@ -1,5 +1,7 @@
 ﻿using FoodFight.Domain.Models;
 using FoodFight.Domain.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,29 +11,65 @@ namespace FoodFight.DAL.Services
 {
     public class GenericDataService<T> : IDataService<T> where T : DomainObject
     {
-        public Task<T> Create(T entity)
+
+        readonly DbContextFactory _dBContextFactory;
+
+        public GenericDataService(DbContextFactory dBContextFactory)
         {
-            throw new NotImplementedException();
+            _dBContextFactory = dBContextFactory;
         }
 
-        public Task<bool> Delete(Guid id)
+        public async Task<T> Create(T entity)
         {
-            throw new NotImplementedException();
+            using (FoodFightDbContext context = _dBContextFactory.CreateDbContext())
+            {
+                EntityEntry<T> createdResult = await context.Set<T>().AddAsync(entity);
+                await context.SaveChangesAsync();
+
+                return createdResult.Entity;
+            }
         }
 
-        public Task<T> Get(Guid id)
+        public async Task<bool> Delete(Guid id)
         {
-            throw new NotImplementedException();
+            using (FoodFightDbContext context = _dBContextFactory.CreateDbContext())
+            {
+                T entity = await context.Set<T>().FirstOrDefaultAsync((e) => e.Id == id);
+                context.Set<T>().Remove(entity);
+                await context.SaveChangesAsync();
+
+                return true;
+            }
         }
 
-        public Task<IEnumerable<T>> GetAll()
+        public async Task<T> Get(Guid id)
         {
-            throw new NotImplementedException();
+            using (FoodFightDbContext context = _dBContextFactory.CreateDbContext())
+            {
+                T entity = await context.Set<T>().FirstOrDefaultAsync((e) => e.Id == id);
+                return entity;
+            }
         }
 
-        public Task<T> Update(Guid id, T entity)
+        public async Task<IEnumerable<T>> GetAll()
         {
-            throw new NotImplementedException();
+            using (FoodFightDbContext context = _dBContextFactory.CreateDbContext())
+            {
+                IEnumerable<T> entities = await context.Set<T>().ToListAsync();
+                return entities;
+            }
+        }
+
+        public async Task<T> Update(Guid id, T entity)
+        {
+            using (FoodFightDbContext context = _dBContextFactory.CreateDbContext())
+            {
+                entity.Id = id;
+                context.Set<T>().Update(entity);
+                await context.SaveChangesAsync();
+
+                return entity;
+            }
         }
     }
 }
